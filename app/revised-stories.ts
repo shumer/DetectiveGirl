8075682046e4b447d00175109f63b911f8737519
Alexander Shumenko <alexander.shumenko@gmail.com>
2026-09-08 20:27:49 +0200

    Rewrite forest case around the failed lantern circuit

--- app/revised-stories.ts
+++ app/revised-stories.ts
@@ -1,4 +1,5 @@
 import type { RevisedStory } from './RevisionCase';
 import { legacyStory } from './legacy-stories';
 import owl from './revised/owl.json';
-export const revisedStories: RevisedStory[] = [owl as RevisedStory,legacyStory('forest'),legacyStory('cloud'),legacyStory('museum')];
+import forest from './revised/forest.json';
+export const revisedStories: RevisedStory[] = [owl as RevisedStory,forest as RevisedStory,legacyStory('cloud'),legacyStory('museum')];
