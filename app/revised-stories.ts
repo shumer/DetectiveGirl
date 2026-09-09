f954dc02ac06bacf6f2a114f77d622c3065518c8
Alexander Shumenko <alexander.shumenko@gmail.com>
2026-09-09 09:00:08 +0200

    Rewrite museum investigation and record reviewed story collection

--- app/revised-stories.ts
+++ app/revised-stories.ts
@@ -1,6 +1,6 @@
 import type { RevisedStory } from './RevisionCase';
-import { legacyStory } from './legacy-stories';
 import owl from './revised/owl.json';
 import forest from './revised/forest.json';
 import cloud from './revised/cloud.json';
-export const revisedStories: RevisedStory[] = [owl as RevisedStory,forest as RevisedStory,cloud as RevisedStory,legacyStory('museum')];
+import museum from './revised/museum.json';
+export const revisedStories: RevisedStory[] = [owl as RevisedStory,forest as RevisedStory,cloud as RevisedStory,museum as RevisedStory];
